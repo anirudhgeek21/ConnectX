c39c407c806eec8d5785062f00c4d7eee13f6748
Anirudh Prajapati <81011908+anirudhgeek21@users.noreply.github.com>
2024-07-12 23:45:16 +0530

    Update Profile.jsx

--- client/src/components/Profile/Profile.jsx
+++ client/src/components/Profile/Profile.jsx
@@ -8,7 +8,7 @@ const ProfilePage = ({ userId, picturePath }) => {
 
   const getUser = async () => {
     try {
-      const response = await fetch(`http://localhost:3001/users/${userId}`, {
+      const response = await fetch(`https://connect-x-backend.vercel.app/users/${userId}`, {
         method: "GET",
         headers: { Authorization: `Bearer ${token}` },
       });
