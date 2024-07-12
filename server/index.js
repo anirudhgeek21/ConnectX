9df74161973b4e4bbdbb7bef3d3eaf99e7c51039
Anirudh Prajapati <81011908+anirudhgeek21@users.noreply.github.com>
2024-07-12 23:49:42 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -25,7 +25,13 @@ app.use(helmet.crossOriginResourcePolicy({ policy: "cross-origin" }));
 app.use(morgan("common"));
 app.use(bodyParser.json({ limit: "30mb", extended: true }));
 app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
-app.use(cors());
+app.use(cors(
+  {
+    origin: ["https://connect-x-xi.vercel.app"],
+    methods: ["POST","GET","PUT","PATCH","DELETE"],
+    credentials: true
+  }
+));
 app.use("/assets", express.static(path.join(__dirname, "public/assets")));
 
 const storage = multer.diskStorage({
